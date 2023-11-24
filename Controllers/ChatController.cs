76c22ea272345f1b767cbde2075a1c4ca18b95eb
Marcelo Augusto <59924047+marceloarc@users.noreply.github.com>
2023-11-23 21:38:58 -0300

    Add files via upload

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -5,8 +5,7 @@ using RenderGallery.Models;
 
 namespace RenderGallery.Controllers
 {
-    [ApiController]
-    [Route("chat")]
+
     public class ChatController : Controller
     {
         private readonly DatabaseContext db;
@@ -16,82 +15,101 @@ namespace RenderGallery.Controllers
             this.db = db;
         }
 
-        [HttpGet("[action]/{id?}")]
-        public async Task<IActionResult> Chat(int Id)
+
+        public IActionResult Chat([FromQuery(Name = "to")] int to, int Id)
         {
+            Console.WriteLine(to + " " + Id);
+            List<Categoria> categorias = db.Categorias.ToList();
+            ViewBag.Categorias = categorias;
             if (Id > 0)
             {
                 var userDetails = db.Users.Where(x => x.Id == Id).FirstOrDefault();
 
-                List<Chat> conversas_query = db.Chats.Where(x => x.user_one == Id || x.user_two == Id).Include(u => u.User1).Include(u => u.User2).ToList();
+                
 
-                var conversas = conversas_query;
+                Chat conversa = db.Chats.Where((x => x.user_one == Id && x.user_two == to)).FirstOrDefault();
 
-                foreach (var conversa in conversas)
+                if(conversa == null)
                 {
-                    if (conversa.User1.Id != userDetails.Id)
+                    conversa = db.Chats.Where((x => x.user_one == to && x.user_two == Id)).FirstOrDefault();
+                    if (conversa == null)
                     {
-                        conversa.User2 = conversa.User1;
-                        conversa.User1 = userDetails;
+                        conversa = new Chat();
+                        conversa.user_one = Id;
+                        conversa.user_two = to;
+                        conversa.time = DateTime.Now;
+                        conversa.status = 0;
+                        db.Chats.Add(conversa);
+                        db.SaveChanges();
+
                     }
                 }
 
-                if (conversas_query != null)
-                {
-                    TempData["chat"] = conversas;
-
-                }
                 if (userDetails != null)
                 {
-                    TempData["user"] = userDetails;
-                   
+                    if(userDetails.Id == Id)
+                    {
+                        conversa.user_one = Id;
+                        conversa.user_two = to;
+                    }
+                    else
+                    {
+                        conversa.user_one = to;
+                        conversa.user_two = Id;
+                    }
                 }
+
+
+                ViewBag.Chat = conversa;
+                ViewBag.from = Id;
+                ViewBag.to = to;
             }
-            return Json(TempData);
+          
+            return View();
         }
 
-        [HttpPost("[action]")]
-        public IActionResult Enviar(Message message)
+
+        public JsonResult Enviar(int from, int? to, string msg, int? cid)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                var id = message.user_id_from;
+                var id = from;
                 var usuario = db.Users.Where(x => x.Id == id).FirstOrDefault();
                 Message mensagem = new Message();
 
                 var conversa = new Chat();
 
-                if (message.chat_id > 0)
+                if (cid > 0)
                 {
-                    conversa = db.Chats.Where(x => x.chat_id == message.chat_id).FirstOrDefault();
+                    conversa = db.Chats.Where(x => x.chat_id == cid).FirstOrDefault();
                 }
                 else
                 {
-                    conversa = db.Chats.Where(x => x.user_one == id && x.user_two == message.user_id_to || x.user_one == message.user_id_to && x.user_two == id).FirstOrDefault();
+                    conversa = db.Chats.Where(x => x.user_one == id && x.user_two == to || x.user_one == to && x.user_two == id).FirstOrDefault();
                 }
 
                 if (conversa == null)
                 {
                     Chat new_conversa = new Chat();
                     new_conversa.user_one = id;
-                    new_conversa.user_two = message.user_id_to;
+                    new_conversa.user_two = to;
                     new_conversa.time = DateTime.Now;
                     new_conversa.status = 0;
                     db.Chats.Add(new_conversa);
                     db.SaveChanges();
-                    message.chat_id = new_conversa.chat_id;
+                    cid = new_conversa.chat_id;
                 }
                 else
                 {
-                    message.chat_id = conversa.chat_id;
+                    cid = conversa.chat_id;
                 }
 
                 mensagem.user_id_from = id;
-                mensagem.msg_content = message.msg_content;
-                mensagem.chat_id = message.chat_id;
+                mensagem.msg_content = msg;
+                mensagem.chat_id = cid;
                 mensagem.dataHora = DateTime.Now;
                 mensagem.visu_status = 0;
-                mensagem.user_id_to = message.user_id_to;
+                mensagem.user_id_to = to;
                 db.Messages.Add(mensagem);
                 db.SaveChanges();
                 TempData["success"] = "sim";
--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
-﻿Insert into users (Name,Email,Password,Telefone,Usuario,Pic) VALUES ('Marcelo','marceloaugusto96@hotmail.com','vidaloka','2323',1,'2fsdf');
-
+﻿Insert into users (Name,Email,Password,Telefone,Usuario,Pic) VALUES ('Marcelo','marceloaugusto96@hotmail.com','vidaloka','2323',1,'images/person_5.jpg');
+Insert into users (Name,Email,Password,Telefone,Usuario,Pic) VALUES ('Pedro','pedro@gmail.com','vidaloka','2323',1,'images/person_6.jpg');
 insert into Categorias (Nome,Image,Descricao) VALUES ('Animes','images/gojo2.png','Animes');
 
 insert into Artistas (User_Id,dataHora) VALUES (1,CONVERT(DATE,'13-12-2019',104));
 
-insert into Publicacoes (Nome,Descricao,"like",deslike,dataHora,Artista_Id) VALUES ('teste','teste',100,100,CONVERT(DATE,'13-12-2019',104),1);
+insert into Publicacoes (Nome,Descricao,dataHora,Artista_Id) VALUES ('teste','teste',CONVERT(DATE,'13-12-2019',104),1);
 
 
-insert into Arts (Arte,Path,Valor,Quantidade,categoria_id,dataHora,PublicacaoId) VALUES ('Teste','images/cu.png',100,100,1,CONVERT(DATE,'13-12-2019',104),1);
+insert into Arts (Arte,Path,Valor,Quantidade,categoria_id,dataHora,publi_id,"like",deslike) VALUES ('Teste','images/cu.png',100,100,1,CONVERT(DATE,'13-12-2019',104),1,10,20);
 
