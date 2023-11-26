e256f4ba1339a30bcd0b582a5f2a3e5890032a10
Marcelo Augusto <59924047+marceloarc@users.noreply.github.com>
2023-11-26 13:53:39 -0300

    Add files via upload

--- data.sql
+++ data.sql
@@ -5,6 +5,6 @@ insert into Categorias (Nome,Image,Descricao) VALUES ('Animes','images/gojo2.png
 insert into Publicacoes (Nome,Descricao,dataHora,User_id) VALUES ('teste','teste',CONVERT(DATE,'13-12-2019',104),2);
 
 
-insert into Arts (Arte,Path,Valor,Quantidade,categoria_id,dataHora,publi_id,"like",deslike) VALUES ('Teste','images/cu.png',100,100,1,CONVERT(DATE,'13-12-2019',104),1,10,20);
+insert into Arts (Arte,Path,Valor,Quantidade,categoria_id,dataHora,publi_id) VALUES ('Teste','images/cu.png',100,100,1,CONVERT(DATE,'13-12-2019',104),1);
 
 
