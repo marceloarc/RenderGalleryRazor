3a787fde1c14192c6812657460b07469ffb3f054
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2023-12-05 17:56:17 -0300

    versão prévia 3

---
+++ Models/LikesDeslikes.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RenderGalleyRazor.Models
+{
+    public class LikesDeslikes
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        public int? user_id { get; set; }
+
+        public int? art_id { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual User? User { get; set; }
+
+        public bool isLike { get; set; }
+
+        public bool isDeslike { get; set; }
+
+        [ForeignKey("art_id")]
+        public virtual Art? Art { get; set; }
+    }
+}
